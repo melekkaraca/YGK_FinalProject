3a3a76242a45762539c8f64e155582f22e733844
Melek Karaca <melekkaraca@gmail.com>
2021-02-06 15:50:09 +0300

    03.02.2021 gün sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,19 +1,28 @@
 ﻿using Business.Concrete;
-using DataAccess.Concrete.EntityFrameWork;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //Open Closed Principle
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EFProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
             foreach (var product in productManager.GetAll())
             {
-                Console.WriteLine(product.ProductName); 
+                Console.WriteLine(product.ProductName);
             }
+            Console.WriteLine();
+            foreach (var product in productManager.GetByUnitPrice(40, 100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+
         }
     }
 }
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //public interface IEntityRepository<T> : Kısılama olmaksızın T tipinde tanımlama
+    //Generic constraint : genel tür kısıtlaması 
+    //class : referans olabilir
+    //IEntity: IEntity olabilir ya da IEntity implemente eden bir nesne olabilir.
+    //new(): new lenebilir olmalı (interfaceler new lenemediği için IEntity olamaz artık)
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
+
